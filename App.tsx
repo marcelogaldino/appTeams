2698a878a8ef479395115921fa574d680a5e8eaa
Marcelo Galdino <marcelo.oliveira@gazin.com.br>
2024-01-15 23:14:27 -0300

    feat: Added Loading component and fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 import { ThemeProvider } from "styled-components/native";
-import theme from "./src/theme";
 import { Groups } from "@screens/Groups";
+import theme from "./src/theme";
+import { Loading } from "@components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
