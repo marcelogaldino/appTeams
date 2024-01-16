654c974111bbca7f00d0cff1599bda2c7772e4d7
Marcelo Galdino <marcelo.oliveira@gazin.com.br>
2024-01-16 08:22:53 -0300

    feat: Header component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,11 @@
 import { Text, View } from "react-native";
 import { Container } from "./styles";
+import { Header } from "@components/Header";
 
 export function Groups() {
   return (
     <Container>
-      <Text>Marcelo</Text>
+      <Header />
     </Container>
   );
 }
