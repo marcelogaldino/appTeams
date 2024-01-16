7ebd2964a89c4545a5135975761b30b7967aaa08
Marcelo Galdino <marcelo.oliveira@gazin.com.br>
2024-01-16 08:11:02 -0300

    style: Statusbar

--- App.tsx
+++ App.tsx
@@ -7,12 +7,18 @@ import { ThemeProvider } from "styled-components/native";
 import { Groups } from "@screens/Groups";
 import theme from "./src/theme";
 import { Loading } from "@components/Loading";
+import { StatusBar } from "react-native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
